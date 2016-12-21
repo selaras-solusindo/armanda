01d96ed0ed4462c989ff2a9464a00c45971f0666
selaras-solusindo <selusin.info@gmail.com>
2016-12-21 19:59:06 +0700

    update tcpdf

--- cetak3.php
+++ cetak3.php
@@ -1,4 +1,71 @@
 <?php
+//============================================================+
+// File name   : example_002.php
+// Begin       : 2008-03-04
+// Last Update : 2013-05-14
+//
+// Description : Example 002 for TCPDF class
+//               Removing Header and Footer
+//
+// Author: Nicola Asuni
+//
+// (c) Copyright:
+//               Nicola Asuni
+//               Tecnick.com LTD
+//               www.tecnick.com
+//               info@tecnick.com
+//============================================================+
+
+/**
+ * Creates an example PDF TEST document using TCPDF
+ * @package com.tecnick.tcpdf
+ * @abstract TCPDF - Example: Removing Header and Footer
+ * @author Nicola Asuni
+ * @since 2008-03-04
+ */
+
+// Include the main TCPDF library (search for installation path).
+require_once('tcpdf_include.php');
+
+// create new PDF document
+$pdf = new TCPDF(PDF_PAGE_ORIENTATION, PDF_UNIT, PDF_PAGE_FORMAT, true, 'UTF-8', false);
+
+// set document information
+$pdf->SetCreator(PDF_CREATOR);
+$pdf->SetAuthor('Armanda');
+$pdf->SetTitle('Invoice');
+$pdf->SetSubject('Invoice');
+$pdf->SetKeywords('TCPDF, PDF, example, test, guide');
+
+// remove default header/footer
+$pdf->setPrintHeader(false);
+$pdf->setPrintFooter(false);
+
+// set default monospaced font
+$pdf->SetDefaultMonospacedFont(PDF_FONT_MONOSPACED);
+
+// set margins
+$pdf->SetMargins(PDF_MARGIN_LEFT, PDF_MARGIN_TOP, PDF_MARGIN_RIGHT);
+
+// set auto page breaks
+$pdf->SetAutoPageBreak(TRUE, PDF_MARGIN_BOTTOM);
+
+// set image scale factor
+$pdf->setImageScale(PDF_IMAGE_SCALE_RATIO);
+
+// set some language-dependent strings (optional)
+if (@file_exists(dirname(__FILE__).'/lang/eng.php')) {
+	require_once(dirname(__FILE__).'/lang/eng.php');
+	$pdf->setLanguageArray($l);
+}
+
+// ---------------------------------------------------------
+
+// set font
+//$pdf->SetFont('times', 'BI', 20);
+
+// add a page
+$pdf->AddPage();
 
 //echo "no_invoice " . $_POST["no_invoice"];
 
@@ -55,5 +122,7 @@ $html .= 'Total setelah PPN '.$total_ppn.'<br>';
 $html .= 'Keterangan '.$keterangan.'<br>';
 $html .= 'Terbilang '.$terbilang.'<br>';
 
-echo $html;
+//echo $html;
+$pdf->writeHTML($html, true, false, true, false, '');
+$pdf->Output('example_006.pdf', 'I');
 ?>
